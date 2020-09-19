318b30bb9e60932c7f6a546c811e4095042c4bc7
Mario Hernandez <mariofix@pm.me>
2020-09-18 22:22:49 -0300

    Stash

--- flow_client.py
+++ flow_client.py
@@ -1,16 +1,33 @@
 from typing import Any, Dict
-from pyflowcl import Payment
+from pyflowcl import Payment, Customer
 from pyflowcl.Clients import ApiClient
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 API_URL = "https://sandbox.flow.cl/api"
-API_KEY = "your_key"
-API_SECRET = "your_secret"
+API_KEY = "5C627F95-4523-4AEB-9FBC-7883B1FL43E5"
+API_SECRET = "43559f1ae777c3f4ff86fb752917356ebf6f2644"
 
 api = ApiClient(API_URL, API_KEY, API_SECRET)
+cust_data: Dict[str, Any] = {
+    "start": 0
+}
 
+llamada = Customer.register(api, 4, "https://mariofix.com")
+print(llamada)
+"""
+llamada = Customer.edit(api, cust_data)
+print(llamada)
+
+llamada = Customer.get(api, "cus_asg7nznrfp")
+print(llamada)
+
+llamada = Customer.get(api, "cus_kpiq2nvif0")
+print(llamada)
+"""
+
+"""
 pago: Dict[str, Any] = {
     "subject": "Asunto Email",
     "commerceOrder": "1234",
@@ -43,3 +60,4 @@ data: Dict[str, Any] = {"apiKey": "", "date": "yyyy-mm-dd"}
 llamada = Payment.getPayments(api, data)
 print(llamada)
 del llamada
+"""
--- pyflowcl/models.py
+++ pyflowcl/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import Any, Dict, Optional, List
+from typing import Any, Dict, Optional, List, cast
 
 
 @dataclass
@@ -271,4 +272,388 @@ class RefundStatus:
             status=status,
             amount=amount,
             fee=fee,
+        )
+
+
+@dataclass
+class Customer:
+    """ Customer Object """
+
+    created: str = ""
+    creditCardType: Optional[str] = None
+    customerId: str = ""
+    email: str = ""
+    externalId: Optional[str] = None
+    last4CardDigits: Optional[str] = None
+    name: str = ""
+    pay_mode: Optional[str] = None
+    registerDate: Optional[str] = None
+    status: int = 0
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> Customer:  # noqa: F821
+        created = d.get("created")
+        creditCardType = d.get("creditCardType")
+        customerId = d.get("customerId")
+        email = d.get("email")
+        externalId = d.get("externalId")
+        last4CardDigits = d.get("last4CardDigits")
+        name = d.get("name")
+        pay_mode = d.get("pay_mode")
+        registerDate = d.get("registerDate")
+        status = d.get("status")
+
+        return Customer(
+            created=created,
+            creditCardType=creditCardType,
+            customerId=customerId,
+            email=email,
+            externalId=externalId,
+            last4CardDigits=last4CardDigits,
+            name=name,
+            pay_mode=pay_mode,
+            registerDate=registerDate,
+            status=status,
+        )
+
+
+@dataclass
+class CustomerRequest:
+    """ CustomerRequest Object """
+
+    apiKey: str = ""
+    customerId: str = ""
+    email: str = ""
+    externalId: str = ""
+    name: str = ""
+    s: str = ""
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CustomerRequest:  # noqa: F821
+        apiKey = d.get("apiKey")
+        customerId = d.get("customerId")
+        email = d.get("email")
+        externalId = d.get("externalId")
+        name = d.get("name")
+        s = d.get("s")
+
+        return CustomerRequest(
+            apiKey=apiKey,
+            customerId=customerId,
+            email=email,
+            externalId=externalId,
+            name=name,
+            s=s,
+        )
+
+
+@dataclass
+class CustomerList:
+    """ Lista de Clientes """
+
+    total: Optional[float] = None
+    hasMore: Optional[bool] = None
+    data: Optional[List[Dict[Any, Any]]] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CustomerList:  # noqa: F821
+        total = d.get("total")
+        hasMore = d.get("hasMore")
+        data = d.get("data")
+
+        return CustomerList(total=total, hasMore=hasMore, data=data,)
+
+
+@dataclass
+class CustomerRegisterResponse:
+    """ Objeto respuesta """
+
+    url: Optional[str] = None
+    token: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CustomerRegisterResponse:  # noqa: F821
+        url = d.get("url")
+        token = d.get("token")
+
+        return CustomerRegisterResponse(url=url, token=token,)
+
+
+@dataclass
+class CustomerRegisterStatusResponse:
+    """ Objeto respuesta """
+
+    creditCardType: str = ""
+    customerId: str = ""
+    last4CardDigits: str = ""
+    status: int = 0
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CustomerRegisterStatusResponse:  # noqa: F821
+        creditCardType = d.get("creditCardType")
+        customerId = d.get("customerId")
+        last4CardDigits = d.get("last4CardDigits")
+        status = d.get("status")
+
+        return CustomerRegisterStatusResponse(
+            creditCardType=creditCardType,
+            customerId=customerId,
+            last4CardDigits=last4CardDigits,
+            status=status,
+        )
+
+
+@dataclass
+class CustomerChargeRequest:
+    """ Objeto para generar una URL de pago """
+
+    amount: float = 0
+    apiKey: str = "API_KEY"
+    commerceOrder: str = ""
+    currency: Optional[str] = None
+    optionals: Optional[str] = None
+    subject: str = ""
+    s: str = ""
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CustomerChargeRequest:  # noqa: F821
+        amount = d.get("amount")
+        apiKey = d.get("apiKey")
+        commerceOrder = d.get("commerceOrder")
+        currency = d.get("currency")
+        optionals = d.get("optionals")
+        subject = d.get("subject")
+        s = d.get("s")
+
+        return CustomerChargeRequest(
+            amount=amount,
+            apiKey=apiKey,
+            commerceOrder=commerceOrder,
+            currency=currency,
+            optionals=optionals,
+            subject=subject,
+            s=s,
+        )
+
+
+@dataclass
+class CollectResponse:
+    """ Objeto para CollectResponse """
+
+    commerce_order: Optional[str] = None
+    flow_order: Optional[float] = None
+    paymen_result: Optional[PaymentStatus] = None
+    status: Optional[int] = None
+    token: Optional[str] = None
+    type: Optional[float] = None
+    url: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CollectResponse:  # noqa: F821
+        type = d.get("type")
+        commerce_order = d.get("commerceOrder")
+        flow_order = d.get("flowOrder")
+        url = d.get("url")
+        token = d.get("token")
+        status = d.get("status")
+        paymen_result = None
+        if d.get("paymenResult") is not None:
+            paymen_result = PaymentStatus.from_dict(
+                cast(Dict[str, Any], d.get("paymenResult"))
+            )
+
+        return CollectResponse(
+            type=type,
+            commerce_order=commerce_order,
+            flow_order=flow_order,
+            url=url,
+            token=token,
+            status=status,
+            paymen_result=paymen_result,
+        )
+
+
+@dataclass
+class CollectRequest:
+    """ Objeto para generar un correo electronico de pago """
+
+    amount: float = 0
+    apiKey: str = "API_KEY"
+    byEmail: Optional[int] = None
+    commerceOrder: str = ""
+    currency: Optional[str] = None
+    customerId: str = ""
+    forward_days_after: Optional[int] = None
+    forward_times: Optional[int] = None
+    ignore_auto_charging: Optional[int] = None
+    merchantId: Optional[str] = None
+    optionals: Optional[str] = None
+    paymentMethod: Optional[int] = 9
+    subject: Optional[str] = None
+    timeout: Optional[int] = None
+    urlConfirmation: str = ""
+    urlReturn: str = ""
+    s: str = ""
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CollectRequest:  # noqa: F821
+        amount = d.get("amount")
+        apiKey = d.get("apiKey")
+        byEmail = d.get("byEmail")
+        commerceOrder = d.get("commerceOrder")
+        currency = d.get("currency")
+        forward_days_after = d.get("forward_days_after")
+        forward_times = d.get("forward_times")
+        ignore_auto_charging = d.get("ignore_auto_charging")
+        merchantId = d.get("merchantId")
+        optionals = d.get("optionals")
+        subject = d.get("subject")
+        timeout = d.get("timeout")
+        urlConfirmation = d.get("urlConfirmation")
+        urlReturn = d.get("urlReturn")
+        s = d.get("s")
+
+        return CollectRequest(
+            amount=amount,
+            apiKey=apiKey,
+            byEmail=byEmail,
+            commerceOrder=commerceOrder,
+            currency=currency,
+            ignore_auto_charging=ignore_auto_charging,
+            forward_days_after=forward_days_after,
+            forward_times=forward_times,
+            merchantId=merchantId,
+            optionals=optionals,
+            subject=subject,
+            timeout=timeout,
+            urlConfirmation=urlConfirmation,
+            urlReturn=urlReturn,
+            s=s,
+        )
+
+
+@dataclass
+class CollectObject:
+    """ Objeto de cobro para un lote de cobros """
+
+    customer_id: str
+    commerce_order: str
+    subject: str
+    amount: float
+    currency: Optional[str] = None
+    payment_method: Optional[float] = None
+    optional: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> CollectObject:  # noqa: F821
+        customer_id = d.get("customerId")
+        commerce_order = d.get("commerceOrder")
+        subject = d.get("subject")
+        amount = d.get("amount")
+        currency = d.get("currency")
+        payment_method = d.get("paymentMethod")
+        optional = d.get("optional")
+
+        return CollectObject(
+            customer_id=customer_id,
+            commerce_order=commerce_order,
+            subject=subject,
+            amount=amount,
+            currency=currency,
+            payment_method=payment_method,
+            optional=optional,
+        )
+
+
+@dataclass
+class BatchCollectRequest:
+    apiKey: str = "API_KEY"
+    batchRows: str = ""
+    byEmail: int = 0
+    forward_days_after: Optional[int] = None
+    forward_times: Optional[int] = None
+    timeout: Optional[int] = None
+    urlCallBack: str = ""
+    urlConfirmation: str = ""
+    urlReturn: str = ""
+    s: str = ""
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> BatchCollectRequest:  # noqa: F821
+        apiKey = d.get("apiKey")
+        batchRows = d.get("batchRows")
+        byEmail = d.get("byEmail")
+        forward_days_after = d.get("forward_days_after")
+        forward_times = d.get("forward_times")
+        timeout = d.get("timeout")
+        urlCallBack = d.get("urlCallBack")
+        urlConfirmation = d.get("urlConfirmation")
+        urlReturn = d.get("urlReturn")
+        s = d.get("s")
+
+        return BatchCollectRequest(
+            apiKey=apiKey,
+            batchRows=batchRows,
+            byEmail=byEmail,
+            forward_days_after=forward_days_after,
+            forward_times=forward_times,
+            timeout=timeout,
+            urlCallBack=urlCallBack,
+            urlConfirmation=urlConfirmation,
+            urlReturn=urlReturn,
+            s=s,
+        )
+
+
+@dataclass
+class BatchCollectRejectedRow:
+    customerId: Optional[str] = None
+    commerceOrder: Optional[str] = None
+    rowNumber: Optional[int] = None
+    parameter: Optional[str] = None
+    errorCode: Optional[int] = None
+    errorMsg: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> BatchCollectRejectedRow:  # noqa: F821
+        customerId = d.get("customerId")
+        commerceOrder = d.get("commerceOrder")
+        rowNumber = d.get("rowNumber")
+        parameter = d.get("parameter")
+        errorCode = d.get("errorCode")
+        errorMsg = d.get("errorMsg")
+
+        return BatchCollectRejectedRow(
+            customerId=customerId,
+            commerceOrder=commerceOrder,
+            rowNumber=rowNumber,
+            parameter=parameter,
+            errorCode=errorCode,
+            errorMsg=errorMsg,
+        )
+
+
+@dataclass
+class BatchCollectResponse:
+    token: Optional[str] = None
+    receivedRows: Optional[int] = None
+    acceptedRows: Optional[int] = None
+    rejectedRows: Optional[List[BatchCollectRejectedRow]] = None
+
+    @staticmethod
+    def from_dict(d: Dict[str, Any]) -> BatchCollectResponse:  # noqa: F821
+        token = d.get("token")
+        receivedRows = d.get("receivedRows")
+        acceptedRows = d.get("acceptedRows")
+        rejectedRows = []
+        for rejected_row in d.get("rejectedRows") or []:
+            rejected_row_item = BatchCollectRejectedRow.from_dict(rejected_row)
+
+            rejectedRows.append(rejected_row_item)
+
+        return BatchCollectResponse(
+            token=token,
+            receivedRows=receivedRows,
+            acceptedRows=acceptedRows,
+            rejectedRows=rejectedRows,
         )
