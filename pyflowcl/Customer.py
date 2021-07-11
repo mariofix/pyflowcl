fa131417be31cc112e5a8a3b1077519973d4ae23
Mario Hernandez <mariofix@pm.me>
2021-07-10 21:29:16 -0400

    Docstrings

--- pyflowcl/Customer.py
+++ pyflowcl/Customer.py
@@ -1,31 +1,31 @@
+"""
+pyflowcl.Customer
+~~~~~~~~~~~~~~~~~
+Modulo para operaciones con Customer
+"""
 from dataclasses import asdict
 from typing import Any, Dict, Union, cast
+from .Clients import ApiClient
+import logging
 
 from .models import (
-    Error,
     Customer,
     CustomerRequest,
+    Error,
     CustomerList,
     CustomerRegisterResponse,
     CustomerRegisterStatusResponse,
     PaymentStatus,
     CustomerChargeRequest,
-    CollectRequest,
     CollectResponse,
-    BatchCollectRequest,
     BatchCollectResponse,
 )
-from .Clients import ApiClient
-import logging
 
 
 def create(
     apiclient: ApiClient, customer_data: Dict[str, Any]
-) -> Union[
-    Customer, Error,
-]:
-    """
-    Permite crear clientes para efectuarles cargos recurrentes o suscribirlos
+) -> Union[Customer, Error]:
+    """Permite crear clientes para efectuarles cargos recurrentes o suscribirlos
     a un planes de suscripción. Una vez creado un cliente, Flow lo identificará
     por un hash denominado customerId, ejemplo:
 
@@ -51,14 +51,8 @@ def create(
         raise Exception(response=response)
 
 
-def edit(
-    apiclient: ApiClient, customer_data: Dict[str, Any]
-) -> Union[
-    Customer, Error,
-]:
-    """
-    Este servicio permite editar los datos de un cliente
-    """
+def edit(apiclient: ApiClient, customer_data: Dict[str, Any]) -> Union[Customer, Error]:
+    """Este servicio permite editar los datos de un cliente"""
     url = f"{apiclient.api_url}/customer/edit"
 
     customer = CustomerRequest.from_dict(customer_data)
@@ -81,12 +75,8 @@ def edit(
 
 def delete(
     apiclient: ApiClient, customer_data: Dict[str, Any]
-) -> Union[
-    Customer, Error,
-]:
-    """
-    Este servicio permite editar los datos de un cliente
-    """
+) -> Union[Customer, Error]:
+    """Este servicio permite editar los datos de un cliente"""
     url = f"{apiclient.api_url}/customer/delete"
 
     customer = CustomerRequest.from_dict(customer_data)
@@ -108,13 +98,10 @@ def delete(
 
 
 def get(
-    apiclient: ApiClient, cust_id: str,
-) -> Union[
-    Customer, Error,
-]:
-    """
-    Permite obtener los datos de un cliente en base a su customerId.
-    """
+    apiclient: ApiClient,
+    cust_id: str,
+) -> Union[Customer, Error]:
+    """Permite obtener los datos de un cliente en base a su customerId."""
     url = f"{apiclient.api_url}/customer/get"
 
     params: Dict[str, Any] = {"apiKey": apiclient.api_key, "customerId": cust_id}
@@ -134,13 +121,10 @@ def get(
 
 
 def list(
-    apiclient: ApiClient, filter_params: Dict[str, Any],
-) -> Union[
-    CustomerList, Error,
-]:
-    """
-    Permite obtener los datos de un cliente en base a su customerId.
-    """
+    apiclient: ApiClient,
+    filter_params: Dict[str, Any],
+) -> Union[CustomerList, Error]:
+    """Permite obtener los datos de un cliente en base a su customerId."""
 
     url = f"{apiclient.api_url}/customer/list"
 
@@ -165,17 +149,14 @@ def list(
 
 def register(
     apiclient: ApiClient, customerId: str, url_return: str
-) -> Union[
-    CustomerRegisterResponse, Error,
-]:
-    """
-    Envía a un cliente a registrar su tarjeta de crédito para poder
+) -> Union[CustomerRegisterResponse, Error]:
+    """Envía a un cliente a registrar su tarjeta de crédito para poder
     efectuarle cargos automáticos. El servicio responde con la URL para
     redirigir el browser del pagador y el token que identifica la
     transacción. La url de redirección se debe formar concatenando los
     valores recibidos en la respuesta de la siguiente forma:
 
-    url + "?token=" +token
+        url + "?token=" +token
 
     Una vez redirigido el browser del cliente, Flow responderá por medio de
     una llamada POST a la url callback del comercio indicada en el parámetro
@@ -210,11 +191,8 @@ def register(
 
 def getRegisterStatus(
     apiclient: ApiClient, token: str
-) -> Union[
-    CustomerRegisterStatusResponse, Error,
-]:
-    """
-    Elte servicio retorna el resultado del registro de la tarjeta de
+) -> Union[CustomerRegisterStatusResponse, Error]:
+    """Este servicio retorna el resultado del registro de la tarjeta de
     crédito de un cliente.
     """
 
@@ -242,13 +220,8 @@ def getRegisterStatus(
         raise Exception(response=response)
 
 
-def unRegister(
-    apiclient: ApiClient, customerId: str
-) -> Union[
-    Customer, Error,
-]:
-    """
-    Este servicio permite eliminar el registro de la tarjeta de crédito
+def unRegister(apiclient: ApiClient, customerId: str) -> Union[Customer, Error]:
+    """Este servicio permite eliminar el registro de la tarjeta de crédito
     de un cliente. Al eliminar el registro no se podrá hacer cargos
     automáticos y Flow enviará un cobro por email.
     """
@@ -277,11 +250,8 @@ def unRegister(
 
 def charge(
     apiclient: ApiClient, charge_data: Dict[str, Any]
-) -> Union[
-    PaymentStatus, Error,
-]:
-    """
-    Este servicio permite efectuar un cargo automático en la tarjeta de
+) -> Union[PaymentStatus, Error]:
+    """Este servicio permite efectuar un cargo automático en la tarjeta de
     crédito previamente registrada por el cliente. Si el cliente no
     tiene registrada una tarjeta el metodo retornará error.
     """
@@ -308,11 +278,8 @@ def charge(
 
 def collect(
     apiclient: ApiClient, collect_data: Dict[str, Any]
-) -> Union[
-    CollectResponse, Error,
-]:
-    """
-    Este servicio envía un cobro a un cliente. Si el cliente tiene
+) -> Union[CollectResponse, Error]:
+    """Este servicio envía un cobro a un cliente. Si el cliente tiene
     registrada una tarjeta de crédito se le hace un cargo automático, si
     no tiene registrada una tarjeta de credito se genera un cobro. Si se
     envía el parámetro byEmail = 1, se genera un cobro por email.
@@ -340,11 +307,8 @@ def collect(
 
 def batchCollect(
     apiclient: ApiClient, collect_data: Dict[str, Any]
-) -> Union[
-    BatchCollectResponse, Error,
-]:
-    """
-    Este servicio envía de forma masiva un lote de cobros a clientes.
+) -> Union[BatchCollectResponse, Error]:
+    """Este servicio envía de forma masiva un lote de cobros a clientes.
     Similar al servicio collect pero masivo y asíncrono. Este servicio
     responde con un token identificador del lote y el número de filas
     recibidas.
